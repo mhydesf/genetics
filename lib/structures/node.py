ec1a83cf06e6a0517670ba9f41bbec74a1bfef6a
Mikhail Hyde <hyde.mikhail@gmail.com>
2023-11-20 16:36:34 -0500

    generalize node definitions

--- lib/structures/node.py
+++ lib/structures/node.py
@@ -1,32 +1,98 @@
 from __future__ import annotations
-from typing import List, Any, Optional
+from abc import ABC, abstractmethod
+from typing import Generic, List, Optional, TypeVar, cast
 
 
-class Node:
-    def __init__(self) -> None:
-        self._parent: Node = Node()
-        self._children: List[Node] = []
-        self._data: Optional[Any] = None
+T = TypeVar("T")
+
+
+class _BaseNode(ABC, Generic[T]):
+    @property
+    @abstractmethod
+    def parent(self) -> _BaseNode:
+        raise NotImplementedError("Cannot call method of abstract class")
+
+    @parent.setter
+    @abstractmethod
+    def parent(self, _: _BaseNode) -> _BaseNode:
+        raise NotImplementedError("Cannot call method of abstract class")
 
     @property
-    def parent(self) -> Node:
+    @abstractmethod
+    def data(self) -> Optional[T]:
+        raise NotImplementedError("Cannot call method of abstract class")
+
+    @data.setter
+    @abstractmethod
+    def data(self, data: T) -> None:
+        raise NotImplementedError("Cannot call method of abstract class")
+
+
+class GeneralNode(_BaseNode[T]):
+    def __init__(self) -> None:
+        self._parent: _BaseNode = cast(_BaseNode, None)
+        self._data: Optional[T] = None
+        self._children: List[_BaseNode] = []
+
+    @property
+    def parent(self) -> _BaseNode:
         return self._parent
 
     @parent.setter
-    def parent(self, parent: Node) -> None:
+    def parent(self, parent: _BaseNode) -> None:
         self._parent = parent
 
     @property
-    def children(self) -> List[Node]:
-        return self._children
-
-    @property
-    def data(self) -> Optional[Any]:
+    def data(self) -> Optional[T]:
         return self._data
 
     @data.setter
-    def data(self, data: Any) -> None:
+    def data(self, data: T) -> None:
         self._data = data
 
-    def add_child(self, child: Node) -> None:
+    @property
+    def children(self) -> List[_BaseNode]:
+        return self._children
+
+    def add_child(self, child: _BaseNode) -> None:
         self._children.append(child)
+
+
+class BinaryNode(_BaseNode[T]):
+    def __init__(self) -> None:
+        self._parent: _BaseNode = cast(_BaseNode, None)
+        self._data: Optional[T] = None
+        self._left_child: Optional[_BaseNode] = None
+        self._right_child: Optional[_BaseNode] = None
+
+    @property
+    def parent(self) -> _BaseNode:
+        return self._parent
+
+    @parent.setter
+    def parent(self, parent: _BaseNode) -> None:
+        self._parent = parent
+
+    @property
+    def data(self) -> Optional[T]:
+        return self._data
+
+    @data.setter
+    def data(self, data: T) -> None:
+        self._data = data
+
+    @property
+    def left_child(self) -> _BaseNode:
+        return cast(_BaseNode, self._left_child)
+
+    @left_child.setter
+    def left_child(self, child: _BaseNode) -> None:
+        self._left_child = child
+
+    @property
+    def right_child(self) -> _BaseNode:
+        return cast(_BaseNode, self._right_child)
+
+    @right_child.setter
+    def right_child(self, child: _BaseNode) -> None:
+        self._right_child = child
