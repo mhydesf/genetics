ee72a8da263d4b1e59c55e688d6cfbc51e02b447
Mikhail Hyde <hyde.mikhail@gmail.com>
2023-11-20 22:54:58 -0500

    add placeholder in solution tree

--- lib/solution/solution_tree.py
+++ lib/solution/solution_tree.py
@@ -1,9 +1,20 @@
 from __future__ import annotations
+from typing import cast
+
+from structures.node import BinaryNode
 from structures.tree import BinaryTree
 from solution.solution_node import SolutionNode
 
 
 class SolutionTree(BinaryTree):
+    def __init__(
+        self,
+        root: SolutionNode
+    ) -> None:
+        self._root = root
+        super().__init__(cast(BinaryNode, self._root))
+
+    # TODO: Implement this
     @staticmethod
     def generate_tree() -> SolutionTree:
-        pass
+        return SolutionTree(SolutionNode(1))
