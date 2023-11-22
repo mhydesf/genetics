50459d5bf791f1ff7bfee5eeef3c579cab42f09e
Mikhail Hyde <hyde.mikhail@gmail.com>
2023-11-21 20:06:57 -0500

    add population class

---
+++ lib/genetics/population.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+from typing import Any, Callable, List
+from structures.solution_node import T
+from structures.solution_tree import SolutionTree
+
+
+class Population:
+    def __init__(
+        self,
+        size: int,
+        solutions: List[SolutionTree],
+    ) -> None:
+        self._size = size
+        self._solutions: List[SolutionTree] = solutions
+
+    @property
+    def size(self) -> int:
+        return self._size
+
+    @property
+    def solutions(self) -> List[SolutionTree]:
+        return self._solutions
+
+    @staticmethod
+    def generate_population(
+        size: int,
+        operators: List[Callable[..., Any]],
+        operands: List[T],
+        max_depth: int,
+    ) -> Population:
+        solutions = [
+            SolutionTree.generate_random_tree(
+                operators=operators,
+                operands=operands,
+                max_depth=max_depth,
+            )
+            for _ in range(size)
+        ]
+        return Population(size, solutions)
