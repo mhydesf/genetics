158ee4ada2db836b16e376d136ad398cec95b974
Mikhail Hyde <hyde.mikhail@gmail.com>
2023-11-23 10:57:07 -0500

    add fitness function to population

--- lib/genetics/population.py
+++ lib/genetics/population.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Any, Callable, List
+from typing import Any, Callable, Dict, List, SupportsFloat, SupportsInt
 from structures.solution_node import T
 from structures.solution_tree import SolutionTree
 
@@ -12,6 +12,7 @@ class Population:
     ) -> None:
         self._size = size
         self._solutions: List[SolutionTree] = solutions
+        self._fitness: Callable[..., Any]
 
     @property
     def size(self) -> int:
@@ -25,6 +26,21 @@ class Population:
     def solutions(self, solutions: List[SolutionTree]) -> None:
         self._solutions = solutions
         self._size = len(solutions)
+
+    @property
+    def fitness(self) -> Callable[..., Any]:
+        return self._fitness
+
+    @fitness.setter
+    def fitness(self, fitness: Callable[..., Any]) -> None:
+        if not callable(fitness):
+            raise TypeError("Fitness funciton must be callable")
+        self._fitness = fitness
+
+    def evaluate_fitness(self) -> Dict[SolutionTree, SupportsInt | SupportsFloat]:
+        if not self.fitness:
+            raise ValueError("Fitness function has not been set")
+        return {solution: self.fitness(solution) for solution in self.solutions}
 
     @staticmethod
     def generate_population(
@@ -44,3 +60,37 @@ class Population:
             for _ in range(size)
         ]
         return Population(size, solutions)
+
+
+if __name__ == "__main__":
+    import math
+    import operator
+    import numpy as np
+    from statistics import mean
+
+    operators = [
+        operator.add,
+        operator.sub,
+        operator.mul,
+        operator.truediv,
+        math.exp,
+        math.pow,
+    ]
+    operands = [1, 2, 3, 4, 5, 'x']
+    max_depth = 3
+
+    population = Population.generate_population(
+        size=10,
+        operators=operators,
+        operands=operands,
+        max_depth=max_depth
+    )
+
+    def minimize_difference(solution: SolutionTree) -> float:
+        diff = []
+        for i in np.linspace(1,10,10):
+            diff.append(i - solution.evaluate(context={'x': i}))
+        return mean(diff)
+
+    population.fitness = minimize_difference
+    fit = population.evaluate_fitness()
